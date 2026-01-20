6ceb76d263f1738028a0996d3d5ef027eed54c2b
IzzulqhaleefFaris <aleeffarees@gmail.com>
2026-01-20 22:57:41 +0800

    creatEvent.php update

--- Org_CreateEvent.php
+++ Org_CreateEvent.php
@@ -154,7 +154,6 @@ endif;
 													<div class="container">
 														<!--begin::Info Asas-->
 														<h5 class="card-title fs-2" style="font-weight: 800">Info Asas</h5>
-														<?php include('userErrors.php'); ?>
 
 														<h3 class="card-title"><small class="text-danger fs-8"> Sila isikan ruang yang bertanda (<i class="text-danger">*</i>&nbsp;)</small></h3>
 
@@ -531,21 +530,6 @@ endif;
 					});
 				}
 			}
-
-			$(document).ready(function() {
-				$("#show_hide_password a").on('click', function(event) {
-					event.preventDefault();
-					if ($('#show_hide_password input').attr("type") == "text") {
-						$('#show_hide_password input').attr('type', 'password');
-						$('#show_hide_password i').addClass("fa-eye-slash");
-						$('#show_hide_password i').removeClass("fa-eye");
-					} else if ($('#show_hide_password input').attr("type") == "password") {
-						$('#show_hide_password input').attr('type', 'text');
-						$('#show_hide_password i').removeClass("fa-eye-slash");
-						$('#show_hide_password i').addClass("fa-eye");
-					}
-				});
-			});
 
 			// Jenis Event Dropdown
 			document.querySelectorAll('.jenis-event-option').forEach(function(item) {
